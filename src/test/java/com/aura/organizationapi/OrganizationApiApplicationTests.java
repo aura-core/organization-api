8512d07e4d1c5e77018aac00be8b9c59da410390
Jonatan Birck <jonatanbirck@gmail.com>
2024-03-10 15:43:29 -0300

    feat: base implementation

---
+++ src/test/java/com/aura/organizationapi/OrganizationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aura.organizationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
